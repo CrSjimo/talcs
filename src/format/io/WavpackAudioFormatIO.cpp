d29cf64b304eeecca098d579ff40f725eccef485
CrSjimo <crs_16423@outlook.com>
2024-09-29 02:43:02 +0800

    add support for wavpack (not finished)

---
+++ src/format/io/WavpackAudioFormatIO.cpp
@@ -0,0 +1,277 @@
+/******************************************************************************
+ * Copyright (c) 2024 CrSjimo                                                 *
+ *                                                                            *
+ * This file is part of TALCS.                                                *
+ *                                                                            *
+ * TALCS is free software: you can redistribute it and/or modify it under the *
+ * terms of the GNU Lesser General Public License as published by the Free    *
+ * Software Foundation, either version 3 of the License, or (at your option)  *
+ * any later version.                                                         *
+ *                                                                            *
+ * TALCS is distributed in the hope that it will be useful, but WITHOUT ANY   *
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS  *
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for    *
+ * more details.                                                              *
+ *                                                                            *
+ * You should have received a copy of the GNU Lesser General Public License   *
+ * along with TALCS. If not, see <https://www.gnu.org/licenses/>.             *
+ ******************************************************************************/
+
+#include "WavpackAudioFormatIO.h"
+#include "WavpackAudioFormatIO_p.h"
+
+#include <limits>
+
+#include <QFileDevice>
+#include <QDebug>
+#include <rpc/msgpack/v1/adaptor/size_equal_only.hpp>
+
+#include <wavpack/wavpack.h>
+
+#define TEST_IS_OPEN(ret)                                                                          \
+    if (!d->context) {                                                                             \
+        qWarning() << "WavpackAudioFormatIO: Not open.";                                           \
+        return ret;                                                                                \
+    }
+
+namespace talcs {
+
+    static inline qint32 wpReadBytes(QFileDevice *stream, void *data, qint32 bcount) {
+        return stream->read(static_cast<char *>(data), bcount);
+    }
+    static inline qint32 wpWriteBytes(QFileDevice *stream, void *data, qint32 bcount) {
+        return stream->write(static_cast<const char *>(data), bcount);
+    }
+    static inline qint64 wpGetPos(const QFileDevice *stream) {
+        return stream->pos();
+    }
+    static inline int wpSetPosAbs(QFileDevice *stream, qint64 pos) {
+        return stream->seek(pos) ? 0 : -1;
+    }
+    static inline int wpSetPosRel(QFileDevice *stream, qint64 delta, int mode) {
+        if (mode == SEEK_SET) {
+            delta += stream->pos();
+        } else if (mode == SEEK_END) {
+            delta += stream->size();
+        }
+        return wpSetPosAbs(stream, delta);
+    }
+    static inline int wpPushBackByte(QFileDevice *stream, int c) {
+        return stream->putChar(static_cast<char>(c)) ? c : -1;
+    }
+    static inline qint64 wpGetLength(const QFileDevice *stream) {
+        return stream->size();
+    }
+    static inline int wpCanSeek(const QFileDevice *stream) {
+        return !stream->isSequential();
+    }
+    static inline int wpTruncateHere(QFileDevice *stream) {
+        return stream->resize(stream->pos()) ? 0 : -1;
+    }
+
+    static WavpackStreamReader64 wpStreamReader = {
+        [](void *id, void *data, int32_t bcount) {
+            return wpReadBytes(static_cast<QFileDevice *>(id), data, bcount);
+        },
+        [](void *id, void *data, int32_t bcount) {
+            return wpWriteBytes(static_cast<QFileDevice *>(id), data, bcount);
+        },
+        [](void *id) {
+            return wpGetPos(static_cast<QFileDevice *>(id));
+        },
+        [](void *id, qint64 pos) {
+            return wpSetPosAbs(static_cast<QFileDevice *>(id), pos);
+        },
+        [](void *id, qint64 pos, int mode) {
+            return wpSetPosRel(static_cast<QFileDevice *>(id), pos, mode);
+        },
+        [](void *id, int c) {
+            return wpPushBackByte(static_cast<QFileDevice *>(id), c);
+        },
+        [](void *id) {
+            return wpGetLength(static_cast<QFileDevice *>(id));
+        },
+        [](void *id) -> int {
+            return wpCanSeek(static_cast<QFileDevice *>(id));
+        },
+        [](void *id) {
+            return wpTruncateHere(static_cast<QFileDevice *>(id));
+        },
+        nullptr,
+    };
+
+    WavpackAudioFormatIO::WavpackAudioFormatIO(QFileDevice *stream, QFileDevice *corrStream) : d_ptr(new WavpackAudioFormatIOPrivate) {
+        Q_D(WavpackAudioFormatIO);
+        d->stream = stream;
+        d->corrStream = corrStream;
+    }
+
+    WavpackAudioFormatIO::~WavpackAudioFormatIO() {
+        WavpackAudioFormatIO::close();
+    }
+
+    void WavpackAudioFormatIO::setStream(QFileDevice *stream, QFileDevice *corrStream) {
+        Q_D(WavpackAudioFormatIO);
+        if (d->openMode) {
+            qWarning() << "WavpackAudioFormatIO: Cannot set stream when WavpackAudioFormatIO is open.";
+            return;
+        }
+        d->stream = stream;
+        d->corrStream = stream;
+    }
+
+    QFileDevice *WavpackAudioFormatIO::stream() const {
+        Q_D(const WavpackAudioFormatIO);
+        return d->stream;
+    }
+
+    QFileDevice *WavpackAudioFormatIO::corrStream() const {
+        Q_D(const WavpackAudioFormatIO);
+        return d->corrStream;
+    }
+
+    bool WavpackAudioFormatIO::open(OpenMode mode) {
+        Q_D(WavpackAudioFormatIO);
+        close();
+        if (mode.testFlag(Read) && mode.testFlag(Write)) {
+            qWarning() << "WavpackAudioFormatIO: Cannot open for both reading and writing";
+            return false;
+        }
+        if (mode == 0) {
+            qWarning() << "WavpackAudioFormatIO: Cannot open because access mode is not specified.";
+            return false;
+        }
+        if (!d->stream) {
+            qWarning() << "WavpackAudioFormatIO: Cannot open because stream is null.";
+            return false;
+        }
+        if (mode.testFlag(Read)) {
+            char err[84];
+            int flags = 0;
+            if (d->corrStream) {
+                flags |= OPEN_WVC;
+            }
+            flags |= (d->threadCount - 1) << OPEN_THREADS_SHFT;
+            auto ctx = WavpackOpenFileInputEx64(&wpStreamReader, d->stream, d->corrStream, err, flags, 0);
+            if (!ctx) {
+                qWarning() << "WavpackAudioFormatIO: Failed to open" << err;
+                return false;
+            }
+            d->context = ctx;
+        } else {
+            // TODO write
+            return false;
+        }
+
+        d->openMode = mode;
+        return true;
+    }
+
+    AbstractAudioFormatIO::OpenMode WavpackAudioFormatIO::openMode() const {
+        Q_D(const WavpackAudioFormatIO);
+        return d->openMode;
+    }
+
+    void WavpackAudioFormatIO::close() {
+        Q_D(WavpackAudioFormatIO);
+        if (d->context) {
+            WavpackCloseFile(d->context);
+            d->context = nullptr;
+        }
+        d->openMode = NotOpen;
+    }
+
+    int WavpackAudioFormatIO::format() const {
+        Q_D(const WavpackAudioFormatIO);
+        TEST_IS_OPEN(0)
+        auto mode = WavpackGetMode(d->context);
+        if (mode & MODE_FLOAT)
+            return Float;
+        return WavpackGetBitsPerSample(d->context);
+    }
+
+    void WavpackAudioFormatIO::setFormat(int format) {
+
+    }
+
+    int WavpackAudioFormatIO::channelCount() const {
+        Q_D(const WavpackAudioFormatIO);
+        TEST_IS_OPEN(0)
+        return WavpackGetNumChannels(d->context);
+    }
+
+    void WavpackAudioFormatIO::setChannelCount(int channelCount) {
+    }
+
+    double WavpackAudioFormatIO::sampleRate() const {
+        Q_D(const WavpackAudioFormatIO);
+        TEST_IS_OPEN(0)
+        return WavpackGetSampleRate(d->context);
+    }
+
+    void WavpackAudioFormatIO::setSampleRate(double sampleRate) {
+    }
+
+    double WavpackAudioFormatIO::bitRate() const {
+        Q_D(const WavpackAudioFormatIO);
+        TEST_IS_OPEN(0)
+        auto mode = WavpackGetMode(d->context);
+        if (mode & MODE_LOSSLESS)
+            return std::numeric_limits<double>::quiet_NaN();
+        return WavpackGetAverageBitrate(d->context, false);
+    }
+
+    void WavpackAudioFormatIO::setBitRate(double bitRate) {
+    }
+
+    void WavpackAudioFormatIO::setThreadCount(int threadCount) {
+        Q_D(WavpackAudioFormatIO);
+        d->threadCount = threadCount;
+    }
+
+    int WavpackAudioFormatIO::threadCount() const {
+        Q_D(const WavpackAudioFormatIO);
+        return d->threadCount;
+    }
+
+    qint64 WavpackAudioFormatIO::length() const {
+        Q_D(const WavpackAudioFormatIO);
+        TEST_IS_OPEN(0)
+        return WavpackGetNumSamples64(d->context);
+    }
+
+    static inline void convertSamplesInPlaceToFloat(float *p, qsizetype size, int bitDepth) {
+        const double NORM = (1 << (bitDepth - 1)) + 0.49999;
+        for (float *pp = p; pp < p + size; pp++) {
+            *p = static_cast<float>(*(reinterpret_cast<qint32 *>(pp)) / NORM);
+        }
+    }
+
+    qint64 WavpackAudioFormatIO::read(float *ptr, qint64 length) {
+        Q_D(WavpackAudioFormatIO);
+        TEST_IS_OPEN(0)
+        auto ret = WavpackUnpackSamples(d->context, reinterpret_cast<qint32 *>(ptr), static_cast<quint32>(length));
+        auto fmt = format();
+        if (fmt == Float) {
+            return ret;
+        } else {
+            convertSamplesInPlaceToFloat(ptr, length * channelCount(), fmt);
+            return ret;
+        }
+    }
+
+    qint64 WavpackAudioFormatIO::write(const float *ptr, qint64 length) {
+        return 0;
+    }
+
+    qint64 WavpackAudioFormatIO::seek(qint64 pos) {
+        Q_D(WavpackAudioFormatIO);
+        return WavpackSeekSample64(d->context, pos);
+    }
+
+    qint64 WavpackAudioFormatIO::pos() const {
+        Q_D(const WavpackAudioFormatIO);
+        return WavpackGetSampleIndex64(d->context);
+    }
+
+} // talcs
---
+++ src/format/io/WavpackAudioFormatIO.h
@@ -0,0 +1,82 @@
+/******************************************************************************
+ * Copyright (c) 2024 CrSjimo                                                 *
+ *                                                                            *
+ * This file is part of TALCS.                                                *
+ *                                                                            *
+ * TALCS is free software: you can redistribute it and/or modify it under the *
+ * terms of the GNU Lesser General Public License as published by the Free    *
+ * Software Foundation, either version 3 of the License, or (at your option)  *
+ * any later version.                                                         *
+ *                                                                            *
+ * TALCS is distributed in the hope that it will be useful, but WITHOUT ANY   *
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS  *
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for    *
+ * more details.                                                              *
+ *                                                                            *
+ * You should have received a copy of the GNU Lesser General Public License   *
+ * along with TALCS. If not, see <https://www.gnu.org/licenses/>.             *
+ ******************************************************************************/
+
+#ifndef TALCS_WAVPACKAUDIOFORMATIO_H
+#define TALCS_WAVPACKAUDIOFORMATIO_H
+
+#include <QScopedPointer>
+
+#include <TalcsFormat/AbstractAudioFormatIO.h>
+
+class QFileDevice;
+
+namespace talcs {
+
+    class WavpackAudioFormatIOPrivate;
+
+    class TALCSFORMAT_EXPORT WavpackAudioFormatIO : public AbstractAudioFormatIO {
+        Q_DECLARE_PRIVATE(WavpackAudioFormatIO)
+    public:
+        explicit WavpackAudioFormatIO(QFileDevice *stream = nullptr, QFileDevice *corrStream = nullptr);
+        ~WavpackAudioFormatIO() override;
+
+        void setStream(QFileDevice *stream, QFileDevice *corrStream = nullptr);
+        QFileDevice *stream() const;
+        QFileDevice *corrStream() const;
+
+        bool open(OpenMode mode) override;
+        OpenMode openMode() const override;
+        void close() override;
+
+        enum WavPackFormat {
+            Unknown = 0x0000,
+            Float = 0x0001,
+            IntMask = 0x00FF,
+        };
+
+        int format() const override;
+        void setFormat(int format) override;
+
+        int channelCount() const override;
+        void setChannelCount(int channelCount) override;
+
+        double sampleRate() const override;
+        void setSampleRate(double sampleRate) override;
+
+        double bitRate() const;
+        void setBitRate(double bitRate);
+
+        void setThreadCount(int threadCount);
+        int threadCount() const;
+
+        qint64 length() const override;
+
+        qint64 read(float *ptr, qint64 length) override;
+        qint64 write(const float *ptr, qint64 length) override;
+
+        qint64 seek(qint64 pos) override;
+        qint64 pos() const override;
+
+    private:
+        QScopedPointer<WavpackAudioFormatIOPrivate> d_ptr;
+    };
+
+}
+
+#endif //TALCS_WAVPACKAUDIOFORMATIO_H
